928a2de0343a236d1ba77f7d49b1ee4c968378fa
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2024-11-01 23:28:27 +0500

    fix: README.md file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <div class="bg-red-500 text-white">
-      <RouterLink to="/" exact-active-class="link-active">Home</RouterLink>
-      <RouterLink to="/about" exact-active-class="link-active"
-        >About</RouterLink
-      >
-    </div>
-  </div>
+  <section class="flex items-center justify-center min-h-screen">
+    hello man
+  </section>
 </template>
