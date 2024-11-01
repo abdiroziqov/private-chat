d3d7dbdced50010e6fe869df51835baac76e10fb
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2024-11-01 23:15:21 +0500

    first commit bismillah

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="bg-red-500 text-white">
+      <RouterLink to="/" exact-active-class="link-active">Home</RouterLink>
+      <RouterLink to="/about" exact-active-class="link-active"
+        >About</RouterLink
+      >
+    </div>
+  </div>
+</template>
