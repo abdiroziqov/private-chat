168116d4c0310c5e1e5427aee0e77d0c89584254
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2024-11-02 10:22:41 +0500

    fix: tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="flex items-center justify-center min-h-screen">
-    hello man
-  </section>
+  <div>
+    <h1 class="text-3xl font-bold my-5 text-center">Private Chat</h1>
+    <section class="flex items-center justify-center min-h-screen">
+      hello man
+    </section>
+  </div>
 </template>
+<script setup lang="ts"></script>
